6e6b5064640ad82ee0ece5c34a32cfc09f734ef7
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-08-03 14:41:16 -0500

    created detailed view for bookmarked images and saves to backend

--- images/models.py
+++ images/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
+
 
 class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -13,7 +15,7 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True,
-                                   db_index=True)
+                               db_index=True)
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
                                         related_name='images_liked',
                                         blank=True)
@@ -25,3 +27,6 @@ class Image(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
